9788e39abd3bc2acffed7c41d26403639871a84f
Yiannis Gatsoulis <y.gatsoulis@leeds.ac.uk>
2016-02-24 02:46:35 +0000

    refactoring, code cleanup

--- py2ipynb.py
+++ py2ipynb.py
@@ -6,7 +6,7 @@ Modified from http://stackoverflow.com/questions/23292242/converting-to-not-from
 
 import argparse
 import nbformat
-from nbformat.v4 import new_code_cell,new_markdown_cell,new_notebook
+from nbformat.v4 import new_code_cell, new_markdown_cell, new_notebook
 import codecs
 from os import linesep
 
@@ -15,22 +15,21 @@ CELLMARKS = {"pycharm": "##",
              "spyder": "#%%"}
 
 
-def parsePy(py_filename, cellmode, other_ignores=[]):
+def parsePy(py_filename, cellmark_style, other_ignores=[]):
     """Converts a .py file to a V.4 .ipynb notebook using special cell markers.
 
     :param py_filename: .py filename
-    :param cellmode: Determines cell marker based on IDE, {"pycharm": "##", "spyder": "#%%"}
+    :param cellmark_style: Determines cell marker based on IDE, {"pycharm": "##", "spyder": "#%%"}
     :param other_ignores: Other lines to ignore
     :return: A string containing one or more lines
     """
-    # ignores = ['"""', "'''"] + [v for k, v in CELLMARKS.items() if k != cellmode] + other_ignores
     ignores = ['"""', "'''"] + CELLMARKS.values() + other_ignores
     with open(py_filename, "r") as f:
         lines = []
         codecell = True
         for l in f:
             l1 = l.strip()
-            if lines and ((l1.startswith('# In[') and l1.endswith(']:')) or l1 == CELLMARKS[cellmode]):
+            if lines and ((l1.startswith('# In[') and l1.endswith(']:')) or l1 == CELLMARKS[cellmark_style]):
                 yield (codecell, "".join(lines).strip(linesep))
                 lines = []
                 codecell = True
@@ -46,17 +45,17 @@ def parsePy(py_filename, cellmode, other_ignores=[]):
         if lines:
             yield (codecell, "".join(lines).strip(linesep))
 
-def py2ipynb(input, output, cellmode, other_ignores=[]):
+def py2ipynb(input, output, cellmark_style, other_ignores=[]):
     """Converts a .py file to a V.4 .ipynb notebook usiing `parsePy` function
 
     :param input: Input .py filename
     :param output: Output .ipynb filename
-    :param cellmode: Determines cell marker based on IDE, see parsePy documentation for values
+    :param cellmark_style: Determines cell marker based on IDE, see parsePy documentation for values
     :param other_ignores: Other lines to ignore
     """
     # Create the code cells by parsing the file in input
     cells = []
-    for c in parsePy(input, cellmode, other_ignores):
+    for c in parsePy(input, cellmark_style, other_ignores):
         codecell, code = c
         cells.append(new_code_cell(source=code) if codecell else new_markdown_cell(source=code))
 
@@ -72,12 +71,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="input python file")
     parser.add_argument("output", help="output notebook file")
-    parser.add_argument("-c", "--cell-mode", help="pycharm|spyder (pycharm)")
+    parser.add_argument("-c", "--cellmark-style", help="pycharm|spyder (pycharm)")
     args = parser.parse_args()
 
-    if not args.cell_mode:
-        args.cell_mode = "pycharm"
+    if not args.cellmark_style:
+        args.cellmark_style = "pycharm"
 
-    py2ipynb(args.input, args.output, args.cell_mode,
+    py2ipynb(args.input, args.output, args.cellmark_style,
              ["# ----------------------------------------------------------------------------"])
 
